62809a5889b014973612b623564e828ae2170c64
Tavish Misra <tmisra2004@gmail.com>
2018-02-09 10:15:57 -0600

    Initial commit

---
+++ src/main/java/com/example/personalbudget/PersonalbudgetApplication.java
@@ -0,0 +1,12 @@
+package com.example.personalbudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalbudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalbudgetApplication.class, args);
+	}
+}
