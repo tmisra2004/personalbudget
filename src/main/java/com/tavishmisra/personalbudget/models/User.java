e272f49807d3d0b0e62f118daf9820a020287ad3
Tavish Misra <tmisra2004@gmail.com>
2018-02-15 16:04:29 -0600

    Finished Password Validator Constraint (not yet tested)

--- src/main/java/com/tavishmisra/personalbudget/models/User.java
+++ src/main/java/com/tavishmisra/personalbudget/models/User.java
@@ -1,4 +1,25 @@
 package com.tavishmisra.personalbudget.models;
 
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private int id;
+    @Column(name = "username")
+    private String username;
+    @Column(name = "password")
+    @Length(min = 8, message = "Your password must be at least 8 characters")
+    @NotEmpty(message = "Please enter a password")
+
+
+
+
 }
