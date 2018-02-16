64f4422d0f87294868e08ab0d373841fc8241f46
Tavish Misra <tmisra2004@gmail.com>
2018-02-15 21:09:21 -0600

    Finished User and Role models

--- src/main/java/com/tavishmisra/personalbudget/models/User.java
+++ src/main/java/com/tavishmisra/personalbudget/models/User.java
@@ -1,9 +1,12 @@
 package com.tavishmisra.personalbudget.models;
 
+import com.tavishmisra.personalbudget.constraint.ValidPassword;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.data.annotation.Transient;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 @Table(name = "user")
@@ -18,8 +21,74 @@ public class User {
     @Column(name = "password")
     @Length(min = 8, message = "Your password must be at least 8 characters")
     @NotEmpty(message = "Please enter a password")
+    @ValidPassword
+    @Transient
+    private String password;
+    @Column(name = "first_name")
+    @NotEmpty(message = "Please enter your first name")
+    private String firstname;
+    @Column(name = "last_name")
+    @NotEmpty(message = "Please enter your last name")
+    private String lastname;
+    @Column(name = "active")
+    private int active;
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    public String getUsername() {
+        return username;
+    }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getActive() {
+        return active;
+    }
+
+    public void setActive(int active) {
+        this.active = active;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
 }
